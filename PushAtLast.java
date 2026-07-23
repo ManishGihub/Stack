91121fa95cd098d011d859130684f5a35b46c221
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-07-23 22:24:53 +0530

    Done

---
+++ PushAtLast.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+//Write a java projgram to push the new element at bottom of stack
+
+public class PushAtLast{
+
+    public static void pushAtLast(int data,Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtLast(data, s);
+        s.push(top);
+    }
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtLast(4, s);
+
+        while(!s.isEmpty()){
+            System.err.println(s.peek());
+            s.pop();
+        }
+    }
+}
+// 3
+// 2
+// 1
+// 4
